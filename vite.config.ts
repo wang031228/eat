ac55ae65088634d0bfa256e8b4e55c81472f6c70
wang031228 <2446284651@qq.com>
2026-06-02 10:51:03 +0800

    修复路径别名 + 重新打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 import tsconfigPaths from "vite-tsconfig-paths";
 import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/eat/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     sourcemap: 'hidden',
   },
